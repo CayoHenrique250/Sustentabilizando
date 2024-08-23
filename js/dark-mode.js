cb1712fccb00b603bd1b1813a695c8af640b205a
CayoHenrique250 <chsf2@aluno.ifal.edu.br>
2024-08-23 16:33:05 -0300

    rem-log-dark

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,6 +1,4 @@
 const chk = document.getElementById('chk');
-const logo = document.getElementById('logo');
-const logo2 = document.getElementById('logo2');
 
 document.addEventListener('DOMContentLoaded', () => {
   const isDarkMode = localStorage.getItem('isDarkMode') === 'true';
@@ -25,12 +23,8 @@ chk.addEventListener('change', () => {
 function setDarkMode(isDarkMode) {
   if (isDarkMode) {
     document.body.classList.add('dark');
-    logo.src = 'img/logo-transparente-clara.png';
-    logo2.src = '../img/logo-transparente-clara.png';
   } else {
     document.body.classList.remove('dark');
-    logo.src = 'img/logo-transparente.png';
-    logo2.src = '../img/logo-transparente.png';
   }
 }
 
