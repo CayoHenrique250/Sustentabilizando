e8336e75857091eb0896e416397652a794fc8a5c
CayoHenrique250 <chsf2@aluno.ifal.edu.br>
2024-08-23 15:26:56 -0300

    remoção para correção

--- js/barra-ferramentas.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("toggleButton").addEventListener("click", function () {
-    var toolbar = document.getElementById("toolbar");
-    if (toolbar.classList.contains("hidden")) {
-        toolbar.classList.remove("hidden");
-    } else {
-        toolbar.classList.add("hidden");
-    }
-});
